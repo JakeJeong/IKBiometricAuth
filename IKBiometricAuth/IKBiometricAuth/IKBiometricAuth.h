1a298f0cded5d5669077577f63cf4d532e42478c
ikjeong <ikjeong@wizvera.com>
2020-09-10 14:56:32 +0900

    # not use comment removed

--- IKBiometricAuth/IKBiometricAuth/IKBiometricAuth.h
+++ IKBiometricAuth/IKBiometricAuth/IKBiometricAuth.h
@@ -33,34 +33,25 @@ typedef void(^EvaluateBlock)(NSString*_Nonnull* _Nonnull reason,NSString *_Nulla
 typedef void(^ResultBlock)(IKBiometricAuthResult * result);
 
 /// 생체인증을 체크하는 클래스
+/// Class to check biometric authentication
 @interface IKBiometricAuth : NSObject
 
-/// 생체인증하기 결과 동기로 받기
+/// 생체 인식 검증 결과를 동기화로 수신
+/// Receive biometric verification results as sync
 @property (class, nonatomic, readonly) IKBiometricAuthResult *(^showAuthentication)(BiometricAuthType, NSString* _Nonnull reason,NSString * _Nullable fallbackTitle);
 
 /// 생체인증하기 블럭함수
+/// Biometric authentication block function
 @property (class, nonatomic, readonly) void(^showAuthenticationWithResult)(IKBiometricAuthResult * _Nullable __strong*_Nullable result,BiometricAuthType,NSString*_Nullable reason,NSString*_Nullable fallbackTitle);
 
 /// 생체인증하기 함수 (결과 블럭으로 받기)
+/// Biometric authentication function (receive as result block)
 @property (class, nonatomic, readonly) void(^showAuthenticationWithResultBlock)(BiometricAuthType,NSString*_Nullable reason,NSString*_Nullable fallbackTitle,ResultBlock resultBlock);
 
 /// 생체인증 타입 리턴 (none, touchid, faceid)
+/// Return biometric authentication type (none, touchid, faceid)
 @property (class, nonatomic, readonly) BiometricType (^BiometricType)(void);
-//
-///// 생체인증 진행하기
-///// @param reason 메세지
-///// @param fallbackTitle 인증 실패후 문구
-//IKBiometricAuthResult* IKBiometricAuthShowAuthentication(NSString*_Nullable reason,NSString*_Nullable fallbackTitle);
-//
-///// 생체인증 진행하고 블럭으로 결과 받기
-///// @param result 결과값 리턴 객체
-///// @param reason 메세지
-///// @param fallbackTitle 인증 실패후 문구
-//void IKBiometricAuthAuthentication(IKBiometricAuthResult * _Nullable __strong*_Nullable result,NSString*_Nullable reason,NSString*_Nullable fallbackTitle);
-//
-//
-////IKBiometricAuthResult* IKBiometricAuthAddCredential(NSString*_Nonnull*_Nonnull alias, NSString*_Nonnull*_Nonnull credentialData);
-////IKBiometricAuthResult* IKBiometricAuthReadCredential(NSString*_Nonnull*_Nonnull alias);
+
 @end
 
 NS_ASSUME_NONNULL_END
